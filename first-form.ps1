3223d11e714ea61ab65e63b9cb3dfe11e382906d
ysftzcn <yusuftezcan@gmail.com>
2019-10-25 13:34:29 +0300

    first form example

---
+++ first-form.ps1
@@ -0,0 +1,13 @@
+# Init Powershell Gui
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create new form
+$localform = New-Object System.Windows.Forms.Form
+
+# Define the size, title and backgorund color
+$localform.ClientSize = '500,300'
+$localform.text = "Yusuf TEZCAN - Powershell GUI"
+
+
+# Display the form
+[void]$localform.ShowDialog()
